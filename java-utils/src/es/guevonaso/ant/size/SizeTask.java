d56afc50ccbf2b40c225c335df3c341fb188c548
fernando.rosado <fernando.rosado@localhost>
2009-02-09 14:00:38 +0000

    Add copyright

--- java-utils/src/es/guevonaso/ant/size/SizeTask.java
+++ java-utils/src/es/guevonaso/ant/size/SizeTask.java
@@ -1,3 +1,21 @@
+/**
+ * 
+ * Copyright (C) 2008 Fernando Rosado Altamirano This file is part of
+ * 'guevonaso-codes' http://code.google.com/p/guevonaso-codes/
+ * 
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3 only, as
+ * published by the Free Software Foundation.
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License version 3 for
+ * more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with this work. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package es.guevonaso.ant.size;
 
 import java.io.File;
