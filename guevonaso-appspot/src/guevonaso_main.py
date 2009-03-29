d885dbc2c5fac07ae9539ad7409e8dfd1aea5f05
fernando.rosado <fernando.rosado@localhost>
2009-03-29 22:24:16 +0000

    Complete Sample

--- guevonaso-appspot/src/guevonaso_main.py
+++ guevonaso-appspot/src/guevonaso_main.py
@@ -1,24 +1,29 @@
 __author__="guevonaso"
 __date__ ="$24-mar-2009 13:53:46$"
-
+import os
 from google.appengine.ext import webapp
 from google.appengine.api import users
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        user = users.get_current_user();
-        if (user):
-            self.response.out.write("Sorry " + user.nickname()+".");
-        self.response.out.write ('Page under construction')
-        url_login = users.create_login_url(self.request.uri)
         
+        user = users.get_current_user();
+        url_login = users.create_login_url(self.request.uri)
+        url_logout = users.create_logout_url(self.request.uri)
+        path = os.path.join(os.path.dirname(__file__), 'templates/sample.html')
+        template_values = {'user':user,
+                           'url_login':url_login,
+                           'url_logout':url_logout
+                           }
+        self.response.out.write(template.render(path, template_values))
+
+
+
         
 application = webapp.WSGIApplication ([('/',MainPage)]
                                       ,debug=True)
-
-
 def main():
     run_wsgi_app(application);
 
