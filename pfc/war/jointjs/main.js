5e8d2dd717cf90c496e104f6bf65cc44dd9cd75d
fernando <fernando@SOBREMESA>
2012-05-08 03:51:51 +0200

    pfc prueba javascript

--- pfc/war/jointjs/main.js
+++ pfc/war/jointjs/main.js
@@ -1,7 +1,7 @@
 var fsa;
 function init() {
-	fsa = Joint.dia.fsa;
-	Joint.paper("world", 800, 400);
+    fsa = Joint.dia.fsa;
+    Joint.paper("world", 800, 400);
 
 }
 
@@ -9,40 +9,49 @@ var estados = new Array();
 var uniones = new Array();
 
 function addState() {
-	var name = document.getElementById("name");
-	if (estados[name.value] != undefined) {
-		var state = estados[name.value];
-		alert("Nombre de estado ya existe : " + state);
-	} else {
-		var state = fsa.State.create({
-			position : {
-				x : 250,
-				y : 100
-			},
-			label : name.value
-		});
-		estados[name.value] = state;
-	}
+    var name = document.getElementById("name");
+    if (estados[name.value] != undefined) {
+        state = estados[name.value];
+        alert("Nombre de estado ya existe : " + state);
+    } else {
+        state = fsa.State.create({
+            position : {
+                x : 250,
+                y : 100
+            },
+            label : name.value
+        });
+        estados[name.value] = state;
+    }
 
 }
 
 function joinStates () {
-	var origen = document.getElementById("origen");
-	var destino = document.getElementById("destino");
-	var union = origen.value + ">>" + destino.value;
-	if (uniones[union] != undefined) { 
-		alert ("Union ya existe" + union);
-	} else {
-		estadoOrigen = estados[origen.value];
-		if (estadoOrigen == undefined) {
-			alert ("Estado origen no existe " + origen.value);
-			return;
-		}
-		estadoDestino = estados[destino.value];
-		if (estadoDestino == undefined) {
-			alert ("Estado destino no existe" + destino.value);
-			return;
-		}
-		estadoOrigen.joint(estadoDestino,fsa.arrow).hideHandle().register(estados);
-	};
-};
+    var origen = document.getElementById("origen");
+    var destino = document.getElementById("destino");
+    var label = document.getElementById("etiqueta");
+    var union = origen.value + ">>" + destino.value;
+    if (uniones[union] != undefined) { 
+        alert ("Union ya existe" + union);
+    } else {
+        estadoOrigen = estados[origen.value];
+        if (estadoOrigen == undefined) {
+            alert ("Estado origen no existe " + origen.value);
+            return;
+        }
+        estadoDestino = estados[destino.value];
+        if (estadoDestino == undefined) {
+            alert ("Estado destino no existe" + destino.value);
+            return;
+        }
+        etiqueta = "";
+        if (label.value != undefined) {
+            etiqueta = label.value;
+        }
+        uniones[union] = etiqueta;
+        estadoOrigen.joint(estadoDestino,
+            (fsa.arrow.label = etiqueta,
+             fsa.arrow)
+            ).hideHandle().register(estados);
+    }
+}
